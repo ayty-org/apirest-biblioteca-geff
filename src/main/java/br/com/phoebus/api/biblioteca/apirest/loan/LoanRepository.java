1c408904434c64d5d20d3f23b08447aa8536f101
Gefferson Vieira <gefferson.vieira@phoebus.local>
2020-03-17 14:32:05 -0300

    Create repositories

---
+++ src/main/java/br/com/phoebus/api/biblioteca/apirest/loan/LoanRepository.java
@@ -0,0 +1,6 @@
+package br.com.phoebus.api.biblioteca.apirest.loan;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanRepository extends JpaRepository<LoanModel, Long> {
+}
---
+++ src/main/java/br/com/phoebus/api/biblioteca/apirest/user/UserRepository.java
@@ -0,0 +1,6 @@
+package br.com.phoebus.api.biblioteca.apirest.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
