e8fca79e0fc8166ad82c8ee0820e1e9043a8ee59
Gefferson Vieira <gefferson.vieira@phoebus.local>
2020-03-25 18:36:29 -0300

    create class UserTest incomplete

---
+++ src/test/java/br/com/phoebus/api/biblioteca/apirest/UserTest/CreateUserServiceTest.java
@@ -0,0 +1,39 @@
+package br.com.phoebus.api.biblioteca.apirest.UserTest;
+
+import br.com.phoebus.api.biblioteca.apirest.exceptions.NotFoundException;
+import br.com.phoebus.api.biblioteca.apirest.user.UserModel;
+import br.com.phoebus.api.biblioteca.apirest.user.UserRepository;
+import br.com.phoebus.api.biblioteca.apirest.user.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Tag("Service")
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Valida o serviço de criar um User")
+public class CreateUserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserModel user;
+
+    private UserService service;
+
+    @BeforeEach
+    void setUp(){
+        this.service = new UserService(userRepository);
+    }
+
+    @Test
+    @DisplayName("Deve criar um user")
+    void shouldCreateUser() throws NotFoundException{
+
+    }
+
+}
