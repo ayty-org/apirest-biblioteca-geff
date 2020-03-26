5599f33c28c4370f8d2788fd02639261a78328f7
Gefferson Vieira <gefferson.vieira@phoebus.local>
2020-03-26 17:34:38 -0300

    add @Repository nos repository

--- src/main/java/br/com/phoebus/api/biblioteca/apirest/loan/LoanRepository.java
+++ src/main/java/br/com/phoebus/api/biblioteca/apirest/loan/LoanRepository.java
@@ -1,7 +1,9 @@
 package br.com.phoebus.api.biblioteca.apirest.loan;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoanRepository extends JpaRepository<LoanModel, Long> {
 
     LoanModel findById(long id);
