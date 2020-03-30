507226ef06a1cc7e8a1b429ac25a8ce5b5dec710
Gefferson Vieira <gefferson.vieira@phoebus.local>
2020-03-30 18:21:17 -0300

    removed BookService and update BookController for new services

--- src/main/java/br/com/phoebus/api/biblioteca/apirest/book/BookController.java
+++ src/main/java/br/com/phoebus/api/biblioteca/apirest/book/BookController.java
@@ -1,9 +1,8 @@
 package br.com.phoebus.api.biblioteca.apirest.book;
 
 
-import br.com.phoebus.api.biblioteca.apirest.book.service.BookService;
+import br.com.phoebus.api.biblioteca.apirest.book.services.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -15,34 +14,40 @@ import java.util.List;
 @RequestMapping("/book")
 public class BookController {
 
-    @Autowired
-    BookService service;
-
-    @GetMapping
-    List<Book> listBooks(){
-        return service.findBooks();
-    }
-
-    @GetMapping("/{id}")
-    Book getBook(@PathVariable(value = "id") long id){
-        return service.findBookById(id);
-    }
+    private final CreateBook createBook;
+    private final DeleteBook deleteBook;
+    private final GetAllBooks getAllBooks;
+    private final GetBook getBook;
+    private final UpdateBook updateBook;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     void postBook(@Validated @RequestBody Book newBook){
-        service.createBook(newBook);
+        createBook.create(newBook);
     }
 
+    //Falar com kawe sobre o metodo delete do repository não ter orElseTrow
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteBookById(@PathVariable(value = "id") long id){
-        service.deleteBookById(id);
+        deleteBook.delete(id);
     }
 
+    @GetMapping
+    List<Book> listBooks(){
+        return getAllBooks.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    Book getBook(@PathVariable(value = "id") long id){
+        return getBook.find(id);
+    }
+
+
+    //Verificar se estou usando certo o DTO aqui
     @PutMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void putBook(@Validated @PathVariable(value = "id")long id, @RequestBody Book attBook){
-        service.updateBook(attBook);
+    void putBook(@Validated @PathVariable(value = "id")long id, @RequestBody BookDTO attBook){
+        updateBook.update(id, attBook);
     }
 }
